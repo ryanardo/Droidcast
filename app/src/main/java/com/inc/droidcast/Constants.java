1b8ef4e3a0c550fd48e8d4804552975d9ccd725c
Ryan G Ferris <ryanglenferris@gmail.com>
2018-03-28 22:39:12 -0700

    API
    Build the API url, and API call.

---
+++ app/src/main/java/com/inc/droidcast/Constants.java
@@ -0,0 +1,6 @@
+package com.inc.droidcast;
+
+public class Constants {
+	public static final String ITUNES_BASE_URL = "https://itunes.apple.com/search?entity=podcast";
+	public static final String ITUNES_PODCAST_QUERY_PARAMETER = "term";
+}
