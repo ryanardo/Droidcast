2fdea8e8be074ae750ad71e86cced3feb4ce74f2
Ryan G Ferris <ryanglenferris@gmail.com>
2018-03-30 16:50:41 -0700

    Constants
    Update the 'Constants' Java class.

--- app/src/main/java/com/inc/droidcast/Constants.java
+++ app/src/main/java/com/inc/droidcast/Constants.java
@@ -3,6 +3,9 @@ package com.inc.droidcast;
 public class Constants {
 	public static final String ITUNES_BASE_URL = "https://itunes.apple.com/search?entity=podcast";
 	public static final String ITUNES_PODCAST_QUERY_PARAMETER = "term";
+
+	public static final String PREFERENCES_PODCAST_KEY = "podcast";
+
 	public static final String FIREBASE_CHILD_PODCAST_SEARCHED = "podcastSearched";
 	public static final String FIREBASE_CHILD_PODCAST_PLAYLIST = "podcastPlaylist";
 	public static final String FIREBASE_CHILD_PODCASTS = "podcasts";
