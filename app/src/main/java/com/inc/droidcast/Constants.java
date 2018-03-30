493a9369bac1edaf745879cced6afdf8e1afb678
Ryan G Ferris <ryanglenferris@gmail.com>
2018-03-30 09:43:56 -0700

    Podcasts Constant
    Create the 'Podcasts' Constant to act as Key in the Firebase database.

--- app/src/main/java/com/inc/droidcast/Constants.java
+++ app/src/main/java/com/inc/droidcast/Constants.java
@@ -3,6 +3,7 @@ package com.inc.droidcast;
 public class Constants {
 	public static final String ITUNES_BASE_URL = "https://itunes.apple.com/search?entity=podcast";
 	public static final String ITUNES_PODCAST_QUERY_PARAMETER = "term";
-	public static final String FIREBASE_CHILD_PODCAST_SEARCHED = "podcastsSearched";
-	public static final String FIREBASE_CHILD_PODCAST_PLAYLIST = "podcastsPlaylist";
+	public static final String FIREBASE_CHILD_PODCAST_SEARCHED = "podcastSearched";
+	public static final String FIREBASE_CHILD_PODCAST_PLAYLIST = "podcastPlaylist";
+	public static final String FIREBASE_CHILD_PODCASTS = "podcasts";
 }
