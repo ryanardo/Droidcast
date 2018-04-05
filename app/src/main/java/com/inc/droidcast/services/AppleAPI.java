8e87f4468c48d0bf56ba59356ed43ac719a80f8b
Ryan G Ferris <ryanglenferris@gmail.com>
2018-04-05 18:47:51 +0000

    Apple API Call
    Build the API query.

--- app/src/main/java/com/inc/droidcast/services/AppleAPI.java
+++ app/src/main/java/com/inc/droidcast/services/AppleAPI.java
@@ -1,12 +1,20 @@
 package com.inc.droidcast.services;
 
 import com.inc.droidcast.Constants;
+import com.inc.droidcast.models.Podcast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class AppleAPI {
 
@@ -28,4 +36,45 @@ public class AppleAPI {
 		call.enqueue(callback);
 	}
 
+	public ArrayList<Podcast> processResults(Response response) {
+		ArrayList<Podcast> podcasts = new ArrayList<>();
+
+		try {
+			String jsonData = response.body().toString();
+			JSONObject podcastJSON = new JSONObject(jsonData);
+			JSONArray podcastsJSON = podcastJSON.getJSONArray("results");
+			for (int i = 0; i < podcastsJSON.length(); i++) {
+				JSONObject podcastJSON = podcastsJSON.getJSONObject(i);
+				int artistId = podcastJSON.getInt("artistId");
+				int collectionId = podcastJSON.getInt("collectionId");
+				int trackId = podcastJSON.getString("trackId");
+				String artistName = podcastJSON.getString("artistName");
+				String collectionName = podcastJSON.getString("collectionName");
+				String trackName = podcastJSON.getString("trackName");
+				String feedUrl = podcastJSON.getString("feedUrl");
+				String trackViewUrl = podcastJSON.getString("trackViewUrl");
+				String releaseDate = podcastJSON.getString("releaseDate");
+				String trackCount = podcastJSON.getString("trackCount");
+				String primaryGenreName = podcastJSON.getString("primaryGenreName");
+				String artworkUrl30 = podcastJSON.getString("artworkUrl30");
+				String artworkUrl60 = podcastJSON.getString("artworkUrl60");
+				String artworkUrl100 = podcastJSON.getString("artworkUrl100");
+				String artworkUrl600 = podcastJSON.getString("artworkUrl600");
+
+				ArrayList<String> genreIds = new ArrayList<>();
+				JSONArray idGenreJSON = podcastJSON.getJSONArray("genreIds");
+				for (int j = 0; j < idGenreJSON.length(); i++) {
+					genreIds.add(idGenreJSON.get(j).toString());
+				}
+
+				ArrayList<String> genres = new ArrayList<>();
+				JSONArray genresJSON = podcastJSON.getJSONArray("genres");
+				for (int j = 0; j < genresJSON.length(); i++) {
+					genres.add(genresJSON.get(j).toString());
+				}
+			}
+		} catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
