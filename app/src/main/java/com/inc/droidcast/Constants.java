3adf0a3e5a55c1f098c3d33d867d606a68d8bce8
Ryan G Ferris <ryanglenferris@gmail.com>
2018-04-12 10:15:08 -0700

    Enviornemtn
    Update project enviornment variables.

--- app/src/main/java/com/inc/droidcast/Constants.java
+++ app/src/main/java/com/inc/droidcast/Constants.java
@@ -9,4 +9,12 @@ public class Constants {
 	public static final String FIREBASE_CHILD_PODCAST_SEARCHED = "podcastSearched";
 	public static final String FIREBASE_CHILD_PODCAST_PLAYLIST = "podcastPlaylist";
 	public static final String FIREBASE_CHILD_PODCASTS = "podcasts";
+	public static final String FIREBASE_QUERY_INDEX = "index";
+
+	public static final String EXTRA_KEY_POSITION = "position";
+	public static final String EXTRA_KEY_PODCASTS = "podcasts";
+
+	public static final String KEY_SOURCE = "source";
+	public static final String SOURCE_SAVED = "saved";
+	public static final String SOURCE_FIND = "find";
 }
