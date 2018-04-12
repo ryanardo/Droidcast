01042584e26da5a4780722891e285e5ea0f8219e
Ryan G Ferris <ryanglenferris@gmail.com>
2018-04-12 10:10:45 -0700

    Adapters
    Create the Podcast List and Podcast Detail Adapters.

--- app/src/main/java/com/inc/droidcast/adapters/PodcastListAdapter.java
+++ app/src/main/java/com/inc/droidcast/adapters/PodcastListAdapter.java
@@ -1,6 +1,10 @@
 package com.inc.droidcast.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,8 +12,14 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.inc.droidcast.Constants;
 import com.inc.droidcast.R;
 import com.inc.droidcast.models.Podcast;
+import com.inc.droidcast.ui.PodcastDetailFragment;
+import com.inc.droidcast.util.OnPodcastSelectedListener;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
@@ -17,23 +27,29 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class PodcastListAdapter extends RecyclerView.Adapter<PodcastListAdapter.PodcastViewHolder> {
+	private static final int MAX_WIDTH = 200;
+	private static final int MAX_HEIGHT = 200;
+
 	private ArrayList<Podcast> mPodcasts = new ArrayList<>();
 	private Context mContext;
+	private OnPodcastSelectedListener mOnPodcastSelectedListener;
 
-	public PodcastListAdapter(Context context, ArrayList<Podcast> podcasts) {
+
+	public PodcastListAdapter(Context context, ArrayList<Podcast> mPodcasts, OnPodcastSelectedListener podcastSelectedListener) {
 		mContext = context;
 		mPodcasts = podcasts;
+		mOnPodcastSelectedListener = podcastSelectedListener;
 	}
 
 	@Override
-	public PodcastViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+	public PodcastListAdapter.PodcastViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.podcast_list_item, parent, false);
-		PodcastViewHolder viewHolder = new PodcastViewHolder(view);
+		PodcastViewHolder viewHolder = new PodcastViewHolder(view, mPodcasts, mOnPodcastSelectedListener);
 		return viewHolder;
 	}
 
 	@Override
-	public void onBindViewHolder(PodcastViewHolder holder, int position) {
+	public void onBindViewHolder(PodcastListAdapter.PodcastViewHolder holder, int position) {
 		holder.bindPodcast(mPodcasts.get(position));
 	}
 
@@ -42,24 +58,67 @@ public class PodcastListAdapter extends RecyclerView.Adapter<PodcastListAdapter.
 		return mPodcasts.size();
 	}
 
-	public class PodcastViewHolder extends RecyclerView.ViewHolder {
-		@BindView(R.id.podcastArtworkUrl100) ImageView mPodcastArtworkUrl100;
-		@BindView(R.id.podcastCollectionName) TextView mPodcastCollectionName;
-		@BindView(R.id.podcastArtistName) TextView mPodcastArtistName;
-		@BindView(R.id.podcastPrimaryGenreName) TextView mPodcastPrimaryGenreName;
+	public class PodcastViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+		@BindView(R.id.podcastArtworkUrl100) ImageView podcastCoverArtwork100;
+		@BindView(R.id.podcastCollectionName) TextView podcastTitle;
+		@BindView(R.id.podcastPrimaryGenreName) TextView podcastGenre;
+		@BindView(R.id.podcastArtistName) TextView podcastArtist;
 
 		private Context mContext;
+		private int mOrientation;
+		private ArrayList<Podcast> mPodcast = new ArrayList<>();
+		private OnPodcastSelectedListener mPodcastSelectedListener;
 
-		public PodcastViewHolder(View itemView) {
+
+		public PodcastViewHolder(View itemView, ArrayList<Podcast> podcasts, OnPodcastSelectedListener podcastSelectedListener) {
 			super(itemView);
 			ButterKnife.bind(this, itemView);
+
 			mContext = itemView.getContext();
+			mOrientation = itemView.getResources().getConfiguration().orientation;
+			mPodcasts = podcasts;
+			mPodcastSelectedListener = podcastSelectedListener;
+
+			if(mOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+				createDetailFragment(0);
+			}
+
+			itemView.setOnClickListener(this);
+
+		}
+
+		@Override
+		public void onClick(View v) {
+			int itemPosition = getLayoutPosition();
+			mPodcastSelectedListener.onPodcastSelected(itemPosition, mPodcast, Constants.SOURCE_FIND);
+			if (mOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+				createDetailFragment(itemPosition);
+			} else {
+				Intent intent = new Intent(mContext, PodcastDetailFragment.class);
+				intent.putExtra(Constants.EXTRA_KEY_POSITION, itemPosition);
+				intent.putExtra(Constants.EXTRA_KEY_PODCASTS, Parcels.wrap(mPodcast));
+				intent.putExtra(Constants.KEY_SOURCE, Constants.SOURCE_FIND);
+				mContext.startActivity(intent);
+			}
+		}
+
+		private void createDetailFragment(int position) {
+			PodcastDetailFragment detailFragment = PodcastDetailFragment.newInstance(mPodcast, position, Constants.SOURCE_FIND);
+			FragmentTransaction ft = ((FragmentActivity)mContext).getSupportFragmentManager().beginTransaction();
+			ft.replace(R.id.podcastDetailContainer, detailFragment);
+			ft.commit();
+
 		}
 
 		public void bindPodcast(Podcast podcast) {
-			mPodcastCollectionName.setText(podcast.getCollectionName());
-			mPodcastArtistName.setText("Artist: " + podcast.getArtistName());
-			mPodcastPrimaryGenreName.setText(podcast.getPrimaryGenreName());
+			Picasso.with(mContext)
+					.load(podcast.getArtworkUrl100())
+					.resize(MAX_WIDTH, MAX_HEIGHT)
+					.centerCrop()
+					.into(podcastCoverArtwork100);
+			podcastTitle.setText(podcast.getCollectionName());
+			podcastGenre.setText(podcast.getPrimaryGenreName());
+			podcastArtist.setText(podcast.getArtistName());
 		}
 	}
 }
