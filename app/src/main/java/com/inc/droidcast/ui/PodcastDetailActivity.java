9c906f60063c87821fa165730760a64e8f772f4d
Ryan G Ferris <ryanglenferris@gmail.com>
2018-04-12 10:09:40 -0700

    Podcast Detail Activity
    Create the Podcast Detail Activity Java Class.

---
+++ app/src/main/java/com/inc/droidcast/ui/PodcastDetailActivity.java
@@ -0,0 +1,42 @@
+package com.inc.droidcast.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.inc.droidcast.R;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PodcastDetailActivity extends AppCompatActivity {
+    private String mSource;
+    @BindView(R.id.viewPager)
+    ViewPager mViewPager;
+    private PodcastPagerAdapter adapterViewPager;
+    ArrayList<Restaurant> mRestaurants = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_detail);
+        ButterKnife.bind(this);
+
+        mSource = getIntent().getStringExtra(Constants.KEY_SOURCE);
+
+        mRestaurants = Parcels.unwrap(getIntent().getParcelableExtra(Constants.EXTRA_KEY_RESTAURANTS));
+        int startingPosition = getIntent().getIntExtra(Constants.EXTRA_KEY_POSITION, 0);
+
+        adapterViewPager = new RestaurantPagerAdapter(getSupportFragmentManager(), mRestaurants, mSource);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
+    }
+}
